56fc85dfc63dab1e1beb4d7ad582f81244e1f80b
GazzaJ <johnge71@gmail.com>
2022-12-31 11:26:21 +0000

    FIX: Corrections to Js having run through JSLint

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const tabs= document.querySelectorAll('.tab')
-const panels = document.querySelectorAll('.panel')
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
-const logo = document.getElementById('logo')
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
 
 
 // Tabs Menu Event Listner
-tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
 
 // Hamburger Button Listner.
-btn.addEventListener('click', navToggle)
+btn.addEventListener('click', navToggle);
 
 function onTabClick(e) {
     // Deactivate all tabs
@@ -18,29 +18,29 @@ function onTabClick(e) {
             'border-softRed',
             'border-b-4',
             'md:border-b-0'
-        )
-    })
+        );
+    });
 
     // Hide all of the panels
-    panels.forEach((panel) => panel.classList.add('hidden'))
+    panels.forEach((panel) => panel.classList.add('hidden'));
 
     // Activate the selected panel
-    e.target.classList.add('border-softRed', 'border-b-4')
-    const classString = e.target.getAttribute('data-target')
+    e.target.classList.add('border-softRed', 'border-b-4');
+    const classString = e.target.getAttribute('data-target');
     document.getElementById('panels')
         .getElementsByClassName(classString)[0]
-        .classList.remove('hidden')
+        .classList.remove('hidden');
 }
 
 // Display the Mobile Menu items
 function navToggle() {
-    btn.classList.toggle('open')
-    menu.classList.toggle('flex')
-    menu.classList.toggle('hidden')
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
 
     if (menu.classList.contains('flex')) {
-        logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+        logo.setAttribute('src', './images/logo-bookmark-footer.svg');
     } else {
-        logo.setAttribute('src', './images/logo-bookmark.svg')
+        logo.setAttribute('src', './images/logo-bookmark.svg');
     }
 }
